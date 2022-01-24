8be79713e69c526de063c0f872162dbe33fbe5ea
MfCambek <fatihcambek@gmail.com>
2022-01-24 22:16:13 +0300

    CategoryRepository başlandı

---
+++ Plugins.DataStore.SQL/CategoryRepository.cs
@@ -0,0 +1,50 @@
+﻿using CoreBusiness;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UseCases.DataStorePluginInterfaces;
+
+namespace Plugins.DataStore.SQL
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly MarketContext db;
+
+        public CategoryRepository(MarketContext db)
+        {
+            this.db = db;
+        }
+        public void AddCategory(Category category)
+        {
+            db.Categories.Add(category);
+            db.SaveChanges();
+        }
+
+        public void DeleteCategory(int categoryId)
+        {
+            var category = db.Categories.Find(categoryId);
+            if (category == null)
+                return;
+
+            db.Categories.Remove(category);
+            db.SaveChanges();
+        }
+
+        public IEnumerable<Category> GetCategories()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Category GetCategoryById(int categoryId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateCategory(Category category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
