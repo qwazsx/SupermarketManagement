a6fb9f58be20f40b9a1b9b264aeed07b9efbf56d
MfCambek <fatihcambek@gmail.com>
2022-01-31 20:33:42 +0300

    Authentication dokunuşları

--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -90,8 +90,12 @@ namespace WebApp
 
             app.UseRouting();
 
+            app.UseAuthentication();
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapRazorPages();
                 endpoints.MapBlazorHub();
                 endpoints.MapFallbackToPage("/_Host");
             });
